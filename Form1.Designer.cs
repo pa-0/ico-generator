65aefccba47372b8402f45c96b622ec2cd138594
KeramidasSoft <anastasios.keramidas@yahoo.com>
2022-05-18 13:28:39 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿
+namespace PngtoIco
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.OpenPng = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SizeIcoBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.FileNameIcon = new System.Windows.Forms.TextBox();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.Dir = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.FilePathName = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // OpenPng
+            // 
+            this.OpenPng.Location = new System.Drawing.Point(24, 22);
+            this.OpenPng.Name = "OpenPng";
+            this.OpenPng.Size = new System.Drawing.Size(75, 23);
+            this.OpenPng.TabIndex = 0;
+            this.OpenPng.Text = "OpenImg";
+            this.OpenPng.UseVisualStyleBackColor = true;
+            this.OpenPng.Click += new System.EventHandler(this.OpenPng_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.LightYellow;
+            this.pictureBox1.Location = new System.Drawing.Point(304, 11);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 500);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.AllowDrop = true;
+            this.panel1.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.panel1.Controls.Add(this.FilePathName);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.Dir);
+            this.panel1.Controls.Add(this.SaveButton);
+            this.panel1.Controls.Add(this.FileNameIcon);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.SizeIcoBox);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(812, 523);
+            this.panel1.TabIndex = 3;
+            this.panel1.DragDrop += new System.Windows.Forms.DragEventHandler(this.panel1_DragDrop);
+            this.panel1.DragEnter += new System.Windows.Forms.DragEventHandler(this.panel1_DragEnter);
+            // 
+            // SizeIcoBox
+            // 
+            this.SizeIcoBox.FormattingEnabled = true;
+            this.SizeIcoBox.Location = new System.Drawing.Point(21, 292);
+            this.SizeIcoBox.Name = "SizeIcoBox";
+            this.SizeIcoBox.Size = new System.Drawing.Size(81, 21);
+            this.SizeIcoBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 273);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Size of the Icon";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 326);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(210, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "File Name of the Icon File without extention";
+            // 
+            // FileNameIcon
+            // 
+            this.FileNameIcon.Location = new System.Drawing.Point(21, 342);
+            this.FileNameIcon.Name = "FileNameIcon";
+            this.FileNameIcon.Size = new System.Drawing.Size(274, 20);
+            this.FileNameIcon.TabIndex = 4;
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(21, 383);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(75, 23);
+            this.SaveButton.TabIndex = 5;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // Dir
+            // 
+            this.Dir.Location = new System.Drawing.Point(21, 129);
+            this.Dir.Name = "Dir";
+            this.Dir.Size = new System.Drawing.Size(75, 23);
+            this.Dir.TabIndex = 6;
+            this.Dir.Text = "....";
+            this.Dir.UseVisualStyleBackColor = true;
+            this.Dir.Click += new System.EventHandler(this.Dir_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Directory";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::PngtoIco.Properties.Resources.sign100;
+            this.pictureBox2.Location = new System.Drawing.Point(197, 434);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(101, 77);
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // FilePathName
+            // 
+            this.FilePathName.Location = new System.Drawing.Point(21, 80);
+            this.FilePathName.Name = "FilePathName";
+            this.FilePathName.Size = new System.Drawing.Size(271, 43);
+            this.FilePathName.TabIndex = 10;
+            this.FilePathName.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(812, 523);
+            this.Controls.Add(this.OpenPng);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "ImgtoIco";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OpenPng;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox SizeIcoBox;
+        private System.Windows.Forms.TextBox FileNameIcon;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.Button Dir;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.RichTextBox FilePathName;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Forms;
+
+namespace PngtoIco
+{
+    public partial class Form1 : Form
+    {
+
+        string directory, nameFile, filePath, LAname = "";
+        bool isFileOpen=false, isDrag=false;
+        double imgWidth, imgHeight;
+        Image img, newImage;
+        string[] sizeIco = { "8x8", "16x16", "32x32", "64x64", "128x128", "256x256", "512x512", "1024x1024" };
+        public Form1()
+        {
+            InitializeComponent();
+            for (int i = 0; i < 8; i++)
+            {
+                SizeIcoBox.Items.Add(sizeIco[i]);
+            }
+            SizeIcoBox.Text = "16x16";
+            FileNameIcon.Text = "icon";
+
+        }
+
+        private void OpenPng_Click(object sender, EventArgs e)
+        {
+            if (isDrag == false)
+            {
+                using (OpenFileDialog openFileDialog = new OpenFileDialog())
+                {
+                    openFileDialog.InitialDirectory = LAname;
+                    openFileDialog.Filter = "image files (*.png, *.bmp, *.gif, *.jpg)|*.png;*.bmp;*.gif;*.jpg";//|All files (*.*)|*.*";
+                    openFileDialog.FilterIndex = 2;
+                    openFileDialog.RestoreDirectory = true;
+
+                    if (openFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        //Get the path of specified file
+                        filePath = openFileDialog.FileName;
+                        directory = System.IO.Path.GetDirectoryName(filePath);
+                        FilePathName.Text = directory;
+                    }
+                }
+            }
+            else
+            {
+
+                filePath = FilePathName.Text;
+                directory = System.IO.Path.GetDirectoryName(filePath);
+                FilePathName.Text = directory;
+            }
+            if (filePath != null)
+            {
+                LAname = directory;
+                Bitmap bitmap = new Bitmap(@filePath);
+                PicinBox(bitmap);
+
+                img = System.Drawing.Image.FromFile(filePath);
+                imgWidth = img.Width;
+                imgHeight = img.Height;
+                MessageBox.Show("Width: " + img.Width + ", Height: " + img.Height);
+
+
+                nameFile = (Path.GetFileName(filePath));
+                Resize(filePath, @directory + "\\new" + nameFile, 500, 500);
+
+                FileStream fs = new FileStream((@directory + "\\new" + nameFile), FileMode.Open, FileAccess.Read);
+                Image imgn = Image.FromStream(fs);
+                fs.Close();
+                bitmap = (Bitmap)imgn.Clone();
+                imgn.Dispose();
+
+                PicinBox(bitmap);
+                File.Delete(@directory + "\\new" + nameFile);
+                isFileOpen = true;
+   
+            }
+            isDrag = false;
+        }
+
+        private void panel1_DragDrop(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Copy;
+        }
+
+        private void panel1_DragEnter(object sender, DragEventArgs e)
+        {
+
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+           foreach(string file in files) FilePathName.Text = file;
+           
+            isDrag = true;
+            OpenPng_Click(sender, e);
+            // ListFiles.Items.Add(file.ToString());
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            if (isFileOpen == true)
+            {
+                nameFile = (Path.GetFileName(filePath));
+                Resize(filePath, @directory + "\\new" + nameFile, 500, 500);
+                FileStream fs = new FileStream((@directory + "\\new" + nameFile), FileMode.Open, FileAccess.Read);
+                Image imgn = Image.FromStream(fs);
+                fs.Close();
+                Bitmap bitmap = (Bitmap)imgn.Clone();
+                imgn.Dispose();
+                PicinBox(bitmap);
+
+                LAname = FilePathName.Text;
+                bool exists = System.IO.Directory.Exists(LAname);
+
+                if (!exists)
+                {
+                    System.IO.Directory.CreateDirectory(LAname);
+                }
+                string outputFileName = FileNameIcon.Text;
+                if (outputFileName == "")
+                {
+                    outputFileName = "icon";
+                    FileNameIcon.Text = "icon";
+                }
+                int size = 16;
+                if (SizeIcoBox.Text == "8x8") size = 8;
+                if (SizeIcoBox.Text == "16x16") size = 16;
+                if (SizeIcoBox.Text == "32x32") size = 32;
+                if (SizeIcoBox.Text == "64x64") size = 64;
+                if (SizeIcoBox.Text == "128x128") size = 128;
+                if (SizeIcoBox.Text == "256x256") size = 256;
+                if (SizeIcoBox.Text == "512x512") size = 512;
+                if (SizeIcoBox.Text == "1024x1024") size = 1024;
+                if (!System.IO.File.Exists(LAname + "\\" + outputFileName + ".ico"))
+                {
+                    ConvertToIcon(@directory + "\\new" + nameFile, LAname + "\\" + outputFileName + ".ico", size, false);
+                    // ConvertToIco(img, LAname + "\\" + outputFileName + ".ico", 500);
+                    MessageBox.Show("The file created successfully in:" + LAname + "\\" + outputFileName + ".ico");
+                }
+                else
+                {
+                    DialogResult d;
+                    d = MessageBox.Show("The file " + nameFile + " EXISTS. Do you want to OVERLAY?", "or No to Cancel", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                    if (d == DialogResult.Yes)
+                    {
+                        ConvertToIcon(@directory + "\\new" + nameFile, LAname + "\\" + outputFileName + ".ico", size, false);
+                        MessageBox.Show("The file created successfully in:" + LAname + "\\" + outputFileName + ".ico");
+                    }
+                    else
+                    {
+                        MessageBox.Show("The file was not created");
+                    }
+                    
+                }
+
+                System.IO.File.Delete(@directory + "\\new" + nameFile);
+                pictureBox1.Update();
+            }
+            else
+            {
+                MessageBox.Show("open a picture");
+            }
+        }
+
+        private void Dir_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FolderBrowserDialog fd1 = new FolderBrowserDialog();
+                //fd1.ShowDialog();
+                if (fd1.ShowDialog() == DialogResult.OK)
+                {
+                    FilePathName.Text = fd1.SelectedPath;
+                    LAname = FilePathName.Text;
+                    directory = FilePathName.Text;
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        public void Resize(string imageFile, string outputFile, int newWidth, int newHeight)
+        {
+            using (var srcImage = Image.FromFile(imageFile))
+            {
+                // var newWidth = (int)(srcImage.Width * scaleFactor);
+                // var newHeight = (int)(srcImage.Height * scaleFactor);
+
+                using (newImage = new Bitmap(newWidth, newHeight))
+                using (var graphics = Graphics.FromImage(newImage))
+                {
+                    graphics.SmoothingMode = SmoothingMode.AntiAlias;
+                    graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                    graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+                    graphics.DrawImage(srcImage, new Rectangle(0, 0, newWidth, newHeight));
+
+
+                    newImage.Save(outputFile);
+                }
+            }
+        }
+
+
+
+      
+        private void PicinBox(Bitmap bitmap)
+        {
+
+            pictureBox1.Image = bitmap;
+            Color pixel5by10 = bitmap.GetPixel(5, 10);
+        }
+
+
+        public static bool ConvertToIcon(string inputPath, string outputPath, int size, bool preserveAspectRatio)
+        {
+           
+            using (FileStream input = new FileStream(inputPath, FileMode.Open, FileAccess.Read))
+            using (FileStream output = new FileStream(outputPath, FileMode.OpenOrCreate))
+            {
+                Bitmap inputBitmap = (Bitmap)Bitmap.FromStream(input);
+                if (inputBitmap != null)
+                {
+                    int width, height;
+                    if (preserveAspectRatio)
+                    {
+                        width = size;
+                        height = inputBitmap.Height / inputBitmap.Width * size;
+                    }
+                    else
+                    {
+                        width = height = size;
+                    }
+                    Bitmap newBitmap = new Bitmap(inputBitmap, new Size(width, height));
+                    if (newBitmap != null)
+                    {
+                        // save the resized png into a memory stream for future use
+                        using (MemoryStream memoryStream = new MemoryStream())
+                        {
+                            newBitmap.Save(memoryStream, ImageFormat.Png);
+
+                            BinaryWriter iconWriter = new BinaryWriter(output);
+                            if (output != null && iconWriter != null)
+                            {
+                                // 0-1 reserved, 0
+                                iconWriter.Write((byte)0);
+                                iconWriter.Write((byte)0);
+
+                                // 2-3 image type, 1 = icon, 2 = cursor
+                                iconWriter.Write((short)1);
+
+                                // 4-5 number of images
+                                iconWriter.Write((short)1);
+
+                                // image entry 1
+                                // 0 image width
+                                iconWriter.Write((byte)width);
+                                // 1 image height
+                                iconWriter.Write((byte)height);
+
+                                // 2 number of colors
+                                iconWriter.Write((byte)0);
+
+                                // 3 reserved
+                                iconWriter.Write((byte)0);
+
+                                // 4-5 color planes
+                                iconWriter.Write((short)0);
+
+                                // 6-7 bits per pixel
+                                iconWriter.Write((short)32);
+
+                                // 8-11 size of image data
+                                iconWriter.Write((int)memoryStream.Length);
+
+                                // 12-15 offset of image data
+                                iconWriter.Write((int)(6 + 16));
+
+                                // write image data
+                                // png data must contain the whole png data file
+                                iconWriter.Write(memoryStream.ToArray());
+
+                                iconWriter.Flush();
+
+                                return true;
+                            }
+                        }
+                    }
+                    return false;
+                }
+                return false;
+            }
+        }
+    }
+}
